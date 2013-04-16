10765f1230c486ceb161b3afb8dd27b5cd8349d5
maglight <maglighter@gmail.com>
2013-04-16 15:27:04 +0400

    finished ddl for oracle and db2

--- orcl/o2d_ddl_4/truncate_tables.sql
+++
@@ -1,5 +0,0 @@
-truncate table O2D_DDL_4.O2D_DDL_4_NUMBERTYPES;
-truncate table O2D_DDL_4.O2D_DDL_4_CHARTYPES;
-truncate table O2D_DDL_4.O2D_DDL_4_DATATYPES; 
-truncate table O2D_DDL_4.O2D_DDL_4_RAWLOBTYPES;
-commit;
--- orcl/o2d_ddl_5/truncate_tables.sql
+++
@@ -1,5 +0,0 @@
-truncate table O2D_DDL_5.O2D_DDL_5_NUMBERTYPES;
-truncate table O2D_DDL_5.O2D_DDL_5_CHARTYPES;
-truncate table O2D_DDL_5.O2D_DDL_5_DATATYPES; 
-truncate table O2D_DDL_5.O2D_DDL_5_RAWLOBTYPES;
-commit;
